2b2e192a6109be89a46b122af299d04c5df5d221
patrice.siffer@hotmail.fr <patrice.siffer@hotmail.fr>
2021-07-14 23:01:23 +0000

    commit

--- DimADAXGeneralJournalAccountEntry.scala
+++ DimADAXGeneralJournalAccountEntry.scala
@@ -1,2 +1,34 @@
 // Databricks notebook source
+import org.apache.spark.sql.functions._
 
+// COMMAND ----------
+
+var dimDF = spark
+  .read
+  .option("header", "true")
+  .parquet("/mnt/datalake/ADAXGeneralJournalAccountEntryStaging/ADAXGeneralJournalAccountEntryStaging*.parquet")
+
+// COMMAND ----------
+
+dimDF = dimDF
+          .select(
+            "ACCOUNTDISPLAYVALUE",
+            "JOURNALNUMBER",
+            "DOCUMENTNUMBER",
+            "ACCOUNTINGDATE",
+            "TRANSACTIONCURRENCYCODE",
+            "TRANSACTIONCURRENCYCREDITAMOUNT",
+            "TRANSACTIONCURRENCYDEBITAMOUNT",
+            "DESCRIPTION",
+            "POSTINGTYPE"
+          )
+          .withColumn("MAINACCOUNT", reverse(reverse($"ACCOUNTDISPLAYVALUE").cast("int").cast("string")))
+
+display(dimDF)
+
+// COMMAND ----------
+
+dimDF
+  .write
+  .option("header", "true")
+  .csv("/mnt/datalake/model/dimension/ADAXGeneralJournalAccountEntry.csv")
